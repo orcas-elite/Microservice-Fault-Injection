89173e2dda5353c55afa9beb14db77612298a611
Jonas Grunert <git@jonas-grunert.de>
2016-12-01 13:50:13 -0800

    Tranparent proxy

--- JInjectProxy/proxy/src/main/java/org/unistuttgart/msinject/proxy/ProxyMain.java
+++ JInjectProxy/proxy/src/main/java/org/unistuttgart/msinject/proxy/ProxyMain.java
@@ -4,9 +4,14 @@ import org.eclipse.jetty.client.api.Request;
 import org.eclipse.jetty.continuation.Continuation;
 import org.eclipse.jetty.continuation.ContinuationSupport;
 import org.eclipse.jetty.proxy.ProxyServlet;
+import org.eclipse.jetty.proxy.ProxyServlet.Transparent;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.FilterHolder;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
+import javax.servlet.DispatcherType;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -16,22 +21,27 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.EnumSet;
 import java.util.Random;
 
-public class ProxyMain extends ProxyServlet { // TODO Transparent
+public class ProxyMain extends Transparent {
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	public void init(ServletConfig config) throws ServletException {
+		System.out.println("proxyTo " + config.getInitParameter("proxyTo"));
 		super.init(config);
-		System.out.println(">> init done !");
+		System.out.println("Proxy init done");
 	}
 
 	@Override
 	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-		System.out.println(">>> got a request !");
+		//System.out.println(">>> got a request !");
 		
-		Random rd = new Random();
-		if(rd.nextBoolean())
-			return;
+		// Drop test
+//		Random rd = new Random();
+//		if(rd.nextBoolean())
+//			return;
 
 		super.service(req, res);
 	}
@@ -39,52 +49,29 @@ public class ProxyMain extends ProxyServlet { // TODO Transparent
 	@Override
 	protected void customizeProxyRequest(Request proxyRequest, HttpServletRequest request) {
 		try {
-			System.out.println("Start wait");
-			Thread.sleep(5000);
-			System.out.println("End wait");
+			//System.out.println("Start wait");
+			Thread.sleep(1000);
+			//System.out.println("End wait");
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		super.customizeProxyRequest(proxyRequest, request);
 	}
 
-	@Override
-	protected URI rewriteURI(HttpServletRequest request) {
-		// URI rewrittenURI = super.rewriteURI(request);
-		//
-		// String apiKeyName = this.getInitParameter("apiKeyName");
-		//
-		// if (apiKeyName != null) {
-		// String fragment = apiKeyName + "=" +
-		// this.getInitParameter("apiKeyValue");
-		//
-		// String queryString = request.getQueryString();
-		//
-		// rewrittenURI += (queryString == null ? "?" : "&") + fragment;
-		//
-		// rewrittenURI = URI.create(rewrittenURI).normalize().toString();
-		// }
-		//
-		// return rewrittenURI;
 
-		try {
-			return new URI("http://0.0.0.0:8080/");
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-	}
-
-	public static void main(String... args) throws Exception {
-
+	public static void main(String... args) throws Exception {  
 		Server server = new Server(8081);
+				
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
 
-		ServletHandler servletHandler = new ServletHandler();
-		servletHandler.addServletWithMapping(ProxyMain.class, "/*");
-
-		server.setHandler(servletHandler);
+		
+		ServletHolder helloServletHolder = new ServletHolder(new ProxyMain());
+		helloServletHolder.setInitParameter("proxyTo", "http://0.0.0.0:8080/");
+		helloServletHolder.setInitParameter("prefix", "/");
+		context.addServlet(helloServletHolder,"/*");
+		
 		server.start();
 		server.join();
 	}
