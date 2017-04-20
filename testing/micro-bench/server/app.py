60cb061db5209f9bc2dabbf20d932e00fd3f3ff9
Jan Strauss <jan.strauss@bigswitch.com>
2017-04-20 15:06:36 +0200

    replace nginx with flask server

---
+++ testing/micro-bench/server/app.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    return jsonify(result=''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(2 ** 16)))
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
